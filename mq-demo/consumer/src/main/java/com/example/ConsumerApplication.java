a65c4c9ba548d31ec70899b0cf593c8d4d63a7ff
hello-github-ui <util.you.come@gmail.com>
2024-01-23 19:33:19 +0800

    #rabbitmq基础# 发送消息

---
+++ mq-demo/consumer/src/main/java/com/example/ConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by 19920728 on 2024/1/23 19:30
+ */
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+}
